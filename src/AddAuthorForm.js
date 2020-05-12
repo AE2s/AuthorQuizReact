aa949820930aabc0c89930b2dc979d0d24593e1f
Boris <anas.essaghir@gmail.com>
2020-05-12 12:18:59 +0200

    Author Quiz application

---
+++ src/AddAuthorForm.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import './AddAuthorForm.css';
+
+class AuthorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      imageUrl: '',
+      books: [],
+      bookTemp: '',
+    };
+
+    this.onFieldChange = this.onFieldChange.bind(this);
+    this.handleAddBook = this.handleAddBook.bind(this);
+  }
+
+  onFieldChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onAddAuthor(this.state);
+  }
+
+  handleAddBook(event) {
+    if (this.state.bookTemp !== '') {
+      this.setState({
+        books: [...this.state.books, this.state.bookTemp],
+        bookTemp: '',
+      });
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={(event) => this.handleSubmit(event)}>
+        <div className="AddAuthorForm__input">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.onFieldChange}
+          />
+        </div>
+
+        <div className="AddAuthorForm__input">
+          <label htmlFor="imageUrl">Image URL</label>
+          <input
+            type="text"
+            name="imageUrl"
+            value={this.state.imageUrl}
+            onChange={this.onFieldChange}
+          />
+        </div>
+        <div className="AddAuthorForm__input">
+          <label htmlFor="bookTemp">Books</label>
+          {this.state.books.map((book) => (
+            <p key={book}>{book}</p>
+          ))}
+          <input
+            type="text"
+            name="bookTemp"
+            value={this.state.bookTemp}
+            onChange={this.onFieldChange}
+          />
+          <input type="button" value="+" onClick={this.handleAddBook} />
+        </div>
+        <input type="submit" value="Add" />
+      </form>
+    );
+  }
+}
+function AddAuthorForm({ match, onAddAuthor }) {
+  return (
+    <div className="AddAuthorForm">
+      <h1>Add Author</h1>
+      <AuthorForm onAddAuthor={onAddAuthor} />
+    </div>
+  );
+}
+
+export default AddAuthorForm;
