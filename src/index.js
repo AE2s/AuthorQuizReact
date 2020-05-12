626e254a0c82d05140d391be547fe32f539d451f
Boris <anas.essaghir@gmail.com>
2020-05-12 14:31:29 +0200

    Add authors

--- src/index.js
+++ src/index.js
@@ -10,38 +10,38 @@ import * as Redux from 'redux';
 import * as ReactRedux from 'react-redux';
 
 const authors = [
-  // {
-  //   name: 'Mark Twain',
-  //   imageUrl: 'images/authors/marktwain.jpg',
-  //   imageSource: 'Wikimedia Commons',
-  //   books: ['The Adventures of Huckleberry Finn'],
-  // },
-  // {
-  //   name: 'Joseph Conrad',
-  //   imageUrl: 'images/authors/josephconrad.png',
-  //   imageSource: 'Wikimedia Commons',
-  //   books: ['Heart of Darkness'],
-  // },
-  // {
-  //   name: 'J.K. Rowling',
-  //   imageUrl: 'images/authors/jkrowling.jpg',
-  //   imageSource: 'Wikimedia Commons',
-  //   imageAttribution: 'Daniel Ogren',
-  //   books: ['Harry Potter and the Sorcerers Stone'],
-  // },
-  // {
-  //   name: 'Stephen King',
-  //   imageUrl: 'images/authors/stephenking.jpg',
-  //   imageSource: 'Wikimedia Commons',
-  //   imageAttribution: 'Pinguino',
-  //   books: ['The Shining', 'IT'],
-  // },
-  // {
-  //   name: 'Charles Dickens',
-  //   imageUrl: 'images/authors/charlesdickens.jpg',
-  //   imageSource: 'Wikimedia Commons',
-  //   books: ['David Copperfield', 'A Tale of Two Cities'],
-  // },
+  {
+    name: 'Mark Twain',
+    imageUrl: 'images/authors/marktwain.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['The Adventures of Huckleberry Finn'],
+  },
+  {
+    name: 'Joseph Conrad',
+    imageUrl: 'images/authors/josephconrad.png',
+    imageSource: 'Wikimedia Commons',
+    books: ['Heart of Darkness'],
+  },
+  {
+    name: 'J.K. Rowling',
+    imageUrl: 'images/authors/jkrowling.jpg',
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Daniel Ogren',
+    books: ['Harry Potter and the Sorcerers Stone'],
+  },
+  {
+    name: 'Stephen King',
+    imageUrl: 'images/authors/stephenking.jpg',
+    imageSource: 'Wikimedia Commons',
+    imageAttribution: 'Pinguino',
+    books: ['The Shining', 'IT'],
+  },
+  {
+    name: 'Charles Dickens',
+    imageUrl: 'images/authors/charlesdickens.jpg',
+    imageSource: 'Wikimedia Commons',
+    books: ['David Copperfield', 'A Tale of Two Cities'],
+  },
   {
     name: 'William Shakespeare',
     imageUrl: 'images/authors/williamshakespeare.jpg',
